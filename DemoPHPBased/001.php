0eb37970d58b39c4980a2aeb626f555997ce6cdb
N9dZhang <fatned@yeah.net>
2015-12-26 20:31:17 +0800

    ned new added

--- DemoPHPBased/001.php
+++ DemoPHPBased/001.php
@@ -1,10 +1,26 @@
 <?php
+
 $a = 12;
 $b = 22;
 $c = 42;
 $d = 0;
+
 $d = ($a>=$b? $a:$b);
 $d = ($d>=$c? $d:$c);
 
 echo ($d);
+echo '<br/>=======';
+//014
+$e = 'super';
+$f = 'man';
+echo '<br/>';
+echo $e , $f; // print the two, so faster than "$e . $f"
+echo '<br/>';
+echo $e . $f; // link the two and print
+
+
+
+
+
+
 ?>
