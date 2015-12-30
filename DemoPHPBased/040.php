cedcae2c0643e494f2aeed81e6282e8db42ae164
N9dZ <fatned@yeah.net>
2015-12-30 17:18:35 +0800

    Signed-off-by: N9dZ <fatned@yeah.net>

--- DemoPHPBased/040.php
+++ DemoPHPBased/040.php
@@ -1,8 +1,14 @@
 <?php
 
-$arr = array(1, 2, 3, false, 4, 5);
-while (list($key, $val) = each($arr)){
-    echo "$key => $val<br>";
+$arr1 = array(1, 2, 3, 4, 5);
+while ($v = current($arr1)) {
+	echo $v, '<br />';
+	next($arr1);
+}
+echo '<br />-#-#-#-#-#-#-#<br />';
+$arr2 = array(1, 2, 3, false, 4, 5);
+while (list($key, $val) = each($arr2)){
+    echo "$val<br />";
 }
 
 ?>
